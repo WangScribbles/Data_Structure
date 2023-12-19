1b3c5caae9bc303812677b295f1bf11a07d5370d
WangScribbles <147413602+WangScribbles@users.noreply.github.com>
2023-12-19 14:22:24 +0800

    Update test.cpp

--- Graph/test.cpp
+++ Graph/test.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "Graph_matrix"
 #include "Graph_table"
+#include "UnionFindSet.h"
+
 using namespace std;
 
 int main()
 {
-	matrix::TestGraph();
-	matrix::TestBDFS();
+	//matrix::TestGraph();
+	//matrix::TestBDFS();
 	
-	link_table::TestGraph();
+	matrix::TestGraphMinTree();
+	
+	//link_table::TestGraph();
 	
 	return 0;
 }
