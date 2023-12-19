47c4e5e6430cf7e6e9433ca576cd262653c1b041
WangScribbles <147413602+WangScribbles@users.noreply.github.com>
2023-12-19 14:23:23 +0800

    Update Graph_matrix.h

--- Graph/Graph_matrix.h
+++ Graph/Graph_matrix.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include <functional>
 
 namespace matrix
 {
@@ -9,7 +10,10 @@ namespace matrix
 template<class V, class W, W MAX_W = INT_MAX, bool Direction = false>
 class Graph
 {
+	typedef Graph<V, W, MAX_W, Direction> Self;
 public:
+	Graph() = default;
+
 	// 图的创建
 	// 1.IO输入  -- 不方便测试，oj中更适合
 	// 2.图结构关系写到文件，读取文件
@@ -51,6 +55,7 @@ public:
 		size_t srci = GetVertexIndex(src);
 		size_t dsti = GetVertexIndex(dst);
 
+		/*
 		_matrix[srci][dsti] = w;
 		
 		// 无向图
@@ -58,6 +63,9 @@ public:
 		{
 			_matrix[dsti][srci] = w;
 		}
+		*/
+		
+		_AddEage(srci, dsti, w);
 	}
 	
 	void Print()
@@ -162,7 +170,95 @@ public:
 		// 不是连通图
 		// 遍历visited数组，然后DFS
 	}
+
+	struct Edge
+	{
+		Edge(size_t srci, size_t dsti, const W& w)
+			:_srci(srci)
+			, _dsti(dsti)
+			, _w(w)
+		{}
+		
+		bool operator>(const Edge& e) const
+		{
+			return _w > e._w;
+		}
+		
+		size_t _srci;
+		size_t _dsti;
+		W _w;
+	};
 	
+	void _AddEage(size_t src, size_t dst, const W& w)
+	{
+		_matrix[srci][dsti] = w;
+		
+		// 无向图
+		if (Direction == false)
+		{
+			_matrix[dsti][srci] = w;
+		}
+	}
+
+	W Kurskal(Self& minTree)
+	{
+		size_t n = _vertexs.size();
+		
+		minTree._vertexs = _vertexs;
+		minTree._indexMap = _indexMap;
+		minTree._matrix.resize(n);
+		for (size_t i = 0; i < n; ++i)
+		{
+			minTree._matrix[i].resize(n, MAX_W);
+		}
+		
+		priority_queue<Edge, vector<Edge>, greater<Edge>> minque;
+		
+		for (size_t i = 0; i < n; ++i)
+		{
+			for (size_t j = 0; j < n; ++j)
+			{
+				if (i < j && _matrix[i][j] != MAX_W)
+				{
+					minque.push(Edge(i, j, _matrix[i][j]));
+				}
+			}
+		}
+		
+		// 选出(n - 1)条边
+		int size = 0;
+		W totalW = W();
+		UinonFindSet ufs(n);
+		while (!minque.empty())
+		{
+			Edge min = minque.top();
+			minque.pop();
+			
+			if (!ufs.InSet(min._srci, min._dsti))
+			{
+				cout << _vertexs[min._srci] << "->" << _vertexs[min._dsti] << ":" << min._w << endl;
+				minTree._AddEage(mid._srci, min._dsti, min._w);
+				ufs.Union(min._srci, min._dsti);
+				++size;
+				totalW += min._w;
+			}
+			else
+			{
+				cout << "构成环了：";
+				cout << _vertexs[min._srci] << "->" << _vertexs[min._dsti] << ":" << min._w << endl;
+			}
+		}
+		
+		if (size == n - 1)
+		{
+			return totalW;
+		}
+		else
+		{
+			return W();
+		}
+	}
+
 private:
 	vector<V>   _vertexs;       // 顶点集合
 	map<V, int> _indexMap;      // 顶点映射下标 
@@ -197,5 +293,34 @@ void TestGraph()
 	g.BFS("张三");
 	//g.DFS("张三");
  }
+
+void TestGraphMinTree()
+{
+     const char* str = "abcdefghi";
+     Graph<char, int> g(str, strlen(str));
+     g.AddEdge('a', 'b', 4);
+     g.AddEdge('a', 'h', 8);
+     //g.AddEdge('a', 'h', 9);
+     g.AddEdge('b', 'c', 8);
+     g.AddEdge('b', 'h', 11);
+     g.AddEdge('c', 'i', 2);
+     g.AddEdge('c', 'f', 4);
+     g.AddEdge('c', 'd', 7);
+     g.AddEdge('d', 'f', 14);
+     g.AddEdge('d', 'e', 9);
+     g.AddEdge('e', 'f', 10);
+     g.AddEdge('f', 'g', 2);
+     g.AddEdge('g', 'h', 1);
+     g.AddEdge('g', 'i', 6);
+     g.AddEdge('h', 'i', 7);
+	 
+     Graph<char, int> kminTree;
+     cout << "Kruskal:" << g.Kruskal(kminTree) << endl;
+     kminTree.Print();
+	 
+     Graph<char, int> pminTree;
+     cout << "Prim:" << g.Prim(pminTree, 'a') << endl;
+     pminTree.Print();
+}
 
 }  // end of namespace matrix
